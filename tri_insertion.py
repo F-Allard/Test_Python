77d79ce14786684fc9064f8bc5d0133224b47e98
Florent <florent.allard@btp-consultants.fr>
2018-10-17 09:51:16 +0200

    1er commit

---
+++ tri_insertion.py
@@ -0,0 +1,16 @@
+liste = [79, 75, 73, 12, 39, 74, 45, 21, 65, 15]
+
+j = 1
+while j < len(liste):
+
+    i = 0
+    while i < j:
+        if liste[j] < liste[i]:
+            liste.insert(i, liste[j])
+
+        else:
+            i = i + 1
+
+    j = j + 1
+
+print(liste)
